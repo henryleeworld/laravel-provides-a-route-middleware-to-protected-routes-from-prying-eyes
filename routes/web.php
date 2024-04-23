d79e19328f33da0a641ffe70c95d491644ed452a
Henry Lee <henryleeworld@gmail.com>
2024-04-24 00:10:07 +0800

    feat:升級至 laravel 9

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/under-construction', function () {
     return view('under-construction');
 });
